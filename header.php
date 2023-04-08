413cd38c6ea599d44579c38826d536eb993f0d13
ronaldos1337 <130254939+ronaldos1337@users.noreply.github.com>
2023-04-09 00:47:48 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+	if ($pun_user['is_admmod'])
+	{
+		$page_statusinfo[] = '<li><span>'.$lang_common['Logged in as'].' <strong style="color:#95B607">'.pun_htmlspecialchars($pun_user['username']).'</strong></span></li>';
+	}
+	else
+
+// don't delete normal user logged in as just add it upper than normal user, if you know PHP you know what i mean :) glhf
+
+// By χ★#7777
